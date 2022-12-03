5a2cb4396fa6c51733750f7c23d9ac37f341cd21
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 07:33:48 +0300

    create database schema

--- db/migrate/20221203041701_create_hero_powers.rb
+++ db/migrate/20221203041701_create_hero_powers.rb
@@ -1,6 +1,8 @@
 class CreateHeroPowers < ActiveRecord::Migration[7.0]
   def change
     create_table :hero_powers do |t|
+      t.belongs_to :hero
+      t.belongs_to :power
 
       t.timestamps
     end
