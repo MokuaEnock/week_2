d506ed328237c790a0381ae77c341e8dc35ceb64
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-03 09:42:34 +0300

    generate create method for hero powers

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,5 +1,12 @@
 class HeroPowersController < ApplicationController
   def create
+    hp = HeroPower.create!(create_params)
+    render json: hp, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: {
+             errors: invalid.record.errors
+           },
+           status: :unprocessable_entity
   end
 
   private
