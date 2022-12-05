1bc033fa586b82e56fa4bd2b0c9730273d6ede05
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 08:36:11 +0300

    add error boundaries to heros

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,6 +1,7 @@
 class HerosController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
   def index
     heros = Hero.all
     render json: heros
@@ -14,6 +15,13 @@ class HerosController < ApplicationController
   private
 
   def render_not_found_response
-    render json: { error: "Hero not found" }, status: :not_found
+    render json: { error: "Restaurant not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             error: invalid.record.errors.full_messages
+           },
+           status: :unprocessable_entity
   end
 end
