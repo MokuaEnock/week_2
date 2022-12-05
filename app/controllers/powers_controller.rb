25270097d59272e38e456d12204c755d8e37d848
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-12-05 08:37:32 +0300

    add status to indx and show methods for heros

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -5,24 +5,18 @@ class PowersController < ApplicationController
 
   def index
     powers = Power.all
-    render json: powers
+    render json: powers, status: :ok
   end
 
   def show
     power = Power.find(params[:id])
-    render json: power
-  end
-
-  def update
-    power = Power.find(params[:id])
-    power.update!(post_params)
     render json: power, status: :ok
   end
 
   private
 
   def render_not_found_response
-    render json: { error: "Pizza not found" }, status: :not_found
+    render json: { error: "Power not found" }, status: :not_found
   end
 
   def render_unprocessable_entity_response(invalid)
